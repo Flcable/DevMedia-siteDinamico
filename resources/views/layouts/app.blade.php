88594b71de7577776fb685d6b6f17c34f96270e9
Flcable <flcable@gmail.com>
2017-02-03 22:42:16 -0200

    Configuração do Bower, Materialize e JQuery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,24 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!--Import Google Icon Font-->
+      <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="{{asset('lib/materialize/dist/css/materialize.css')}}">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Laravel</title>
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="/css/app.css" rel="stylesheet">
-
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
 </head>
 <body>
     <div id="app">
@@ -82,6 +72,7 @@
     </div>
 
     <!-- Scripts -->
-    <script src="/js/app.js"></script>
+     <script src="{{asset('lib/jquery/dist/jquery.js')}}"></script>
+    <script src="{{asset('lib/materialize/dist/js/materialize.js')}}"></script>
 </body>
 </html>
